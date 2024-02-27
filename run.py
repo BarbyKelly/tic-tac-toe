19bf443d61c7c8d0babdb18dd89add121ca83cc2
Barby Kelly <barbykelly1@gmail.com>
2024-02-27 13:07:19 +0000

    Set up steps for creating the game

--- run.py
+++ run.py
@@ -1,1 +1,47 @@
-# Creating Tic-Tac-Toe game
+# Overview of the game
+
+# Create Welcome for the player
+# Option to read instructions (i), play the game (p) or exit (e). "Type in the letter of your choice (i, p, or e) and then press Enter. 
+# Create Error messages if they choose anything else than i, p, e. Letters only, and those letters only.
+
+# Instructions:
+# This version of Tic-Tac-Toe involves 2 players: website visitor vs computer and Tic-Tac-Toe board consisting 9 empty boxes.
+# Before the game starts visitor will be asked to enter their initials.
+# Computer's moves are marked with 0.
+# Visitor's moves will be marked with an X.
+# Both players take turns to mark the boxes on Tic-Tac-Toe board with their symbols.
+# Visitor can mark their chosen empty box on the board with a number between 1-9, and then press Enter to confirm their choice.
+# Each box can be chosen only once during each game.
+# Goal of Tic-Tac-Toe is to get 3 of the same symbols in a straight line: vertically, horizontally or diagonally.
+# Player who achieves to align their 3 symbols first in a straight continues line, is the winner.
+# Game is over when one of the players wins, or when all of the boxes are filled, and game ends up in a tie.
+# Once game is over, visitor has a choice to play again, exit the game or go over instructions again if they wish to do so.
+
+# Create an option for the player to enter their name up to 10 characters or just initials? ("Please press 'Enter' after typing your name or initials")
+# Create Error messages if not letters, if using symbols, if longer than 10 characters. Can you have an alert if they haven't pressed Enter after certain amount
+# of time?
+
+# Create Tic-Tac-Toe board with 1-9 empty boxes
+# Choose if lines are needed around the boxes or does it look better without the lines. 
+
+# Allocate 0 for computer and X for the visitor
+
+# Game starts when visitor chooses their first box to be marked with their symbol
+# Does visitor always start or can it be set on random, importing random?
+
+# Create turn-taking: visitor, computer, visitor, computer, until one wins or it's a tie
+# Create an Error message if visitor enters other than 1-9 number
+# Create an Error message if visitor tries to fill a box/enter the number, that is already taken by the visitor or by the computer
+
+# Create congratulations notice for when visitor wins
+# Create a message: Better luck next time! when visitor loses
+# Create a message: "It's a tie! Would you like to play again (p), go over instructions (i) or exit the game (e)? Make sure to press
+# Enter after you have made your choice." Would it be possible to play, exit etc without pressing Enter?
+
+# Create a message for all of the options: if visitor exits the game in the end or at the start, have an option to quit between or to go back to the instructions?
+# Create notice of what happens if quitting during the game or if going back to instructions, new game will start when they return. Game can't be paused, saved?
+
+# Exit the game. Have a message show up: "Thank you for visiting my Tic-Tac-Toe game site."
+
+# Future development or this project: Add a score system "Visitor's name(initials) vs computer", add different symbols, add a time-limit, add an option best of 3 is the overall
+# winner or best of 5, best of 10 
