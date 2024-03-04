06deb0fd0641e5dd0756bd640f691b8fd10620d9
Barby Kelly <barbykelly1@gmail.com>
2024-03-04 09:58:30 +0000

    Edit is_winner function, error. Fix white spaces according to CI Python Linter. Fixes durint mentor meeting.

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ def free_space(pos):
     return board[pos] == ' '
 
 
-def tictactoe_board(board):
+def tictactoe_board():
     print('+--+--+--+--')
     print('| 1 | 2 | 3 |')
     print('| ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
@@ -30,17 +30,19 @@ def tictactoe_board(board):
     print('| ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('|  |  |  |')
 
+
 # Create a condition to determine the winner
 def is_winner(bo, le):
-    return
-    (board[1] == le and bo[2] == le and bo[3] == le)
-    or (board[4] == le and bo[5] == le and bo[6] == le)
-    or (board[7] == le and bo[8] == le and bo[9] == le)
-    or (board[1] == le and bo[4] == le and bo[7] == le)
-    or (board[2] == le and bo[5] == le and bo[8] == le)
-    or (board[3] == le and bo[6] == le and bo[9] == le)
-    or (board[1] == le and bo[5] == le and bo[9] == le)
-    or (board[7] == le and bo[5] == le and bo[3] == le)
+    return (
+        (board[1] == le and bo[2] == le and bo[3] == le) or
+        (board[4] == le and bo[5] == le and bo[6] == le) or
+        (board[7] == le and bo[8] == le and bo[9] == le) or
+        (board[1] == le and bo[4] == le and bo[7] == le) or
+        (board[2] == le and bo[5] == le and bo[8] == le) or
+        (board[3] == le and bo[6] == le and bo[9] == le) or
+        (board[1] == le and bo[5] == le and bo[9] == le) or
+        (board[7] == le and bo[5] == le and bo[3] == le)
+    )
 
 
 # Create player's move, guidelines and restrictions
@@ -76,7 +78,7 @@ def comp_move():
 
     corner_open = []
     for i in poss_move:
-        if i in [1,3,7,9]:
+        if i in [1, 3, 7, 9]:
             corner_open.append(i)
 
     if len(corner_open) > 0:
@@ -89,7 +91,7 @@ def comp_move():
 
     edges_open = []
     for i in poss_move:
-        if i in [2,4,6,8]:
+        if i in [2, 4, 6, 8]:
             edges_open.append(i)
 
     if len(edges_open) > 0:
@@ -101,7 +103,7 @@ def comp_move():
 # Add random function for computer's moves
 def select_random(li):
     ln = len(li)
-    r = random.randrange(o,ln)
+    r = random.randrange(o, ln)
     return li(r)
 
 
@@ -116,7 +118,7 @@ def is_board_full(board):
 # Create Welcome for the player. Add input option after - their name
 # Add conditions when board is printed and when not
 def main():
-    print("Welcome to Tic Tac Toe")
+    print("Welcome to Tic Tac Toe \U000FEB68")
     print(tictactoe_board)
 
     while not (is_board_full(board)):
@@ -131,60 +133,16 @@ def main():
             move = comp_move()
             if move == 0:
                 print("Tie! Thanks for playing!")
+            else:
+                insert_letter('O', board)
+                print('Computer placed an \'O\' in position', move, ':')
+                print_board(board)
         else:
-            insert_letter('O', board)
-            print('Computer placed an \'O\' in position', move , ':')
-            print_board(board)
-    else:
-        print("Congratulations! You won! Thanks for playing!")
-        break
+            print("Congratulations! You won! Thanks for playing!")
+            break
 
     if is_board_full(board):
         print("Tie! Thanks for playing!")
+        
 
-    
-
-# Option to read instructions (i), play the game (p) or exit (e). "Type in the letter of your choice (i, p, or e) and then press Enter. 
-# Create Error messages if they choose anything else than i, p, e. Letters only, and those letters only.
-
-# Instructions:
-# This version of Tic-Tac-Toe involves 2 players: website visitor vs computer and Tic-Tac-Toe board consisting 9 empty boxes.
-# Before the game starts visitor will be asked to enter their initials.
-# Computer's moves are marked with 0.
-# Visitor's moves will be marked with an X.
-# Both players take turns to mark the boxes on Tic-Tac-Toe board with their symbols.
-# Visitor can mark their chosen empty box on the board with a number between 1-9, and then press Enter to confirm their choice.
-# Each box can be chosen only once during each game.
-# Goal of Tic-Tac-Toe is to get 3 of the same symbols in a straight line: vertically, horizontally or diagonally.
-# Player who achieves to align their 3 symbols first in a straight continues line, is the winner.
-# Game is over when one of the players wins, or when all of the boxes are filled, and game ends up in a tie.
-# Once game is over, visitor has a choice to play again, exit the game or go over instructions again if they wish to do so.
-
-# Create an option for the player to enter their name up to 10 characters or just initials? ("Please press 'Enter' after typing your name or initials")
-# Create Error messages if not letters, if using symbols, if longer than 10 characters. Can you have an alert if they haven't pressed Enter after certain amount
-# of time?
-
-# Create Tic-Tac-Toe board with 1-9 empty boxes
-# Choose if lines are needed around the boxes or does it look better without the lines. 
-
-# Allocate 0 for computer and X for the visitor
-
-# Game starts when visitor chooses their first box to be marked with their symbol
-# Does visitor always start or can it be set on random, importing random?
-
-# Create turn-taking: visitor, computer, visitor, computer, until one wins or it's a tie
-# Create an Error message if visitor enters other than 1-9 number
-# Create an Error message if visitor tries to fill a box/enter the number, that is already taken by the visitor or by the computer
-
-# Create congratulations notice for when visitor wins
-# Create a message: Better luck next time! when visitor loses
-# Create a message: "It's a tie! Would you like to play again (p), go over instructions (i) or exit the game (e)? Make sure to press
-# Enter after you have made your choice." Would it be possible to play, exit etc without pressing Enter?
-
-# Create a message for all of the options: if visitor exits the game in the end or at the start, have an option to quit between or to go back to the instructions?
-# Create notice of what happens if quitting during the game or if going back to instructions, new game will start when they return. Game can't be paused, saved?
-
-# Exit the game. Have a message show up: "Thank you for visiting my Tic-Tac-Toe game site."
-
-# Future development or this project: Add a score system "Visitor's name(initials) vs computer", add different symbols, add a time-limit, add an option best of 3 is the overall
-# winner or best of 5, best of 10 
+main()
